c7597262a7e0df25ed4dbec72fc77f2d846cfbd5
Mayte Steeghs <M.C.Steeghs@student.tudelft.nl>
2022-12-07 14:20:52 +0100

    create framework code for microservices
    Using the context map and the example microservice provided by the course we created the appropriate microservices
    
    
    update yml for gitlab ci/cd pipeline
    
    
    remove intellij files
    
    
    add example tests back to microservices

---
+++ requests-microservice/src/main/java/nl/tudelft/sem/template/requests/Application.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.template.requests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * requests microservice application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
