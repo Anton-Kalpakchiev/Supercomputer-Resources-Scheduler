1770ec1a1072f86e489c68665b8b57608d021b5e
Sophie van der Linden <s.a.vanderlinden@student.tudelft.nl>
2022-12-07 20:39:29 +0100

    Created repository

---
+++ schedule-microservice/src/main/java/nl/tudelft/sem/template/schedule/domain/ScheduleRepository.java
@@ -0,0 +1,16 @@
+package nl.tudelft.sem.template.schedule.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<DailySchedule, DailyScheduleId> {
+    /**
+     * Find requests for given day and resource pool
+     */
+    Optional<DailySchedule> findByDayAndResourcePool(Date day, long resourcePoolId);
+
+}
