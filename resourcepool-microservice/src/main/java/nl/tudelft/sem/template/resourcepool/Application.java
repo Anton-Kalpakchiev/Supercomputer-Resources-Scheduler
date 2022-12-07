714b25a978af607c8b1964bd3b4616ae806ecd25
Mayte Steeghs <m.c.steeghs@student.tudelft.nl>
2022-12-07 16:15:21 +0100

    Create Microservices Framework

---
+++ resourcepool-microservice/src/main/java/nl/tudelft/sem/template/resourcepool/Application.java
@@ -0,0 +1,14 @@
+package nl.tudelft.sem.template.resourcepool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * resourcepool microservice application.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
