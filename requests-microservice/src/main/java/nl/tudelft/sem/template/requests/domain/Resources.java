b5255bc9b3a88100085815630e22c81407674604
Wilhelm Marcu <w.p.a.marcu@student.tudelft.nl>
2022-12-08 17:36:28 +0100

    Added base classes for resources and resource request model

---
+++ requests-microservice/src/main/java/nl/tudelft/sem/template/requests/domain/Resources.java
@@ -0,0 +1,37 @@
+package nl.tudelft.sem.template.requests.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+/**
+ * A DDD value object representing a password in our domain.
+ */
+@EqualsAndHashCode
+public class Resources {
+    @Getter
+    private final transient int mem;
+    @Getter
+    private final transient int cpu;
+    @Getter
+    private final transient int gpu;
+
+    /**
+     * A container class for holding different types of resources.
+     *
+     * @param mem The number of memory resources.
+     *
+     * @param cpu The number of CPU resources.
+     *
+     * @param gpu The number of GPU resources.
+     */
+    public Resources(int mem, int cpu, int gpu) {
+        this.mem = mem;
+        this.cpu = cpu;
+        this.gpu = gpu;
+    }
+
+    @Override
+    public String toString() {
+        return "Memory: " + mem + " CPU: " + cpu + " GPU: " + gpu;
+    }
+}
