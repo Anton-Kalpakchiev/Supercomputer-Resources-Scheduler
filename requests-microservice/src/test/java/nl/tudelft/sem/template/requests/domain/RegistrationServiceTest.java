fdbea1f6b899a7195f292ec58e4e68e4c52c9fa7
Wilhelm Marcu <w.p.a.marcu@student.tudelft.nl>
2022-12-09 22:32:10 +0100

    Added test class for registration service

---
+++ requests-microservice/src/test/java/nl/tudelft/sem/template/requests/domain/RegistrationServiceTest.java
@@ -0,0 +1,43 @@
+package nl.tudelft.sem.template.requests.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+// activate profiles to have spring use mocks during auto-injection of certain beans.
+@ActiveProfiles({"test"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class RegistrationServiceTest {
+
+    @Autowired
+    private transient RegistrationService registrationService;
+
+    @Autowired
+    private transient RequestRepository requestRepository;
+
+    @Test
+    public void createRequest_withValidData_worksCorrectly() throws Exception {
+        // Arrange
+        final String description = "give me resources";
+        final Resources resources = new Resources(30, 50, 50);
+
+        // Act
+        registrationService.registerRequest(description, resources);
+
+        // Assert
+        AppRequest savedRequest = requestRepository.findById(0L).orElseThrow();
+
+        assertThat(savedRequest.getDescription()).isEqualTo(description);
+        assertThat(savedRequest.getMem()).isEqualTo(resources.getMem());
+        assertThat(savedRequest.getCpu()).isEqualTo(resources.getCpu());
+        assertThat(savedRequest.getGpu()).isEqualTo(resources.getGpu());
+    }
+}
