9af26ff87b9c7637129bd48db1e3aae08330bab1
Mayte Steeghs <M.C.Steeghs@student.tudelft.nl>
2022-12-23 20:59:03 +0100

    fix registration service test

--- requests-microservice/src/test/java/nl/tudelft/sem/template/requests/domain/RegistrationServiceTest.java
+++ requests-microservice/src/test/java/nl/tudelft/sem/template/requests/domain/RegistrationServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -64,29 +65,29 @@ class RegistrationServiceTest {
         token = "token";
     }
 
-    //    @Test
-    //    public void createRequest_withValidData_worksCorrectly() throws InvalidResourcesException {
-    //        // Arrange
-    //
-    //        when(mockResourcePoolService.approval(
-    //                any(Calendar.class), any(long.class), false, any(String.class))).thenReturn(
-    //                ResponseEntity.ok(true));
-    //
-    //        // Act
-    //        AppRequest returnedRequest = registrationService.registerRequest(description, resources, owner,
-    //                facultyName, availableResources, deadline, freePoolResources, token);
-    //
-    //        // Assert
-    //        AppRequest savedRequest = requestRepository.findById(returnedRequest.getId()).orElseThrow();
-    //
-    //        assertThat(savedRequest.getDescription()).isEqualTo(description);
-    //        assertThat(savedRequest.getMem()).isEqualTo(resources.getMemory());
-    //        assertThat(savedRequest.getCpu()).isEqualTo(resources.getCpu());
-    //        assertThat(savedRequest.getGpu()).isEqualTo(resources.getGpu());
-    //        assertThat(savedRequest.getOwner()).isEqualTo(owner);
-    //        assertThat(savedRequest.getFacultyName()).isEqualTo(facultyName);
-    //        assertThat(savedRequest.getDeadline()).isEqualTo(deadline);
-    //    }
+    @Test
+    public void createRequest_withValidData_worksCorrectly() throws InvalidResourcesException {
+        // Arrange
+
+        when(mockResourcePoolService.approval(
+                any(Calendar.class), any(long.class), eq(false), any(String.class))).thenReturn(
+                ResponseEntity.ok(true));
+
+        // Act
+        AppRequest returnedRequest = registrationService.registerRequest(description, resources, owner,
+                facultyName, availableResources, deadline, freePoolResources, token);
+
+        // Assert
+        AppRequest savedRequest = requestRepository.findById(returnedRequest.getId()).orElseThrow();
+
+        assertThat(savedRequest.getDescription()).isEqualTo(description);
+        assertThat(savedRequest.getMem()).isEqualTo(resources.getMemory());
+        assertThat(savedRequest.getCpu()).isEqualTo(resources.getCpu());
+        assertThat(savedRequest.getGpu()).isEqualTo(resources.getGpu());
+        assertThat(savedRequest.getOwner()).isEqualTo(owner);
+        assertThat(savedRequest.getFacultyName()).isEqualTo(facultyName);
+        assertThat(savedRequest.getDeadline()).isEqualTo(deadline);
+    }
 
     @Test
     public void createRequest_withNegativeMemory_throwsException() {
