afd60d08ba9ec19d7dd4ac0d0190e14bf8974752
Mayte Steeghs <M.C.Steeghs@student.tudelft.nl>
2023-01-12 20:41:00 +0100

    Refactor tests to reflect new class inheritance structure

--- users-microservice/src/test/java/nl/tudelft/sem/template/users/integration/FacadeControllerTest.java
+++ users-microservice/src/test/java/nl/tudelft/sem/template/users/integration/FacadeControllerTest.java
@@ -25,14 +25,16 @@ import nl.tudelft.sem.template.users.domain.FacultyVerificationService;
 import nl.tudelft.sem.template.users.domain.RegistrationService;
 import nl.tudelft.sem.template.users.domain.Sysadmin;
 import nl.tudelft.sem.template.users.domain.SysadminRepository;
+import nl.tudelft.sem.template.users.facade.NodesRequestService;
 import nl.tudelft.sem.template.users.facade.RequestSenderService;
+import nl.tudelft.sem.template.users.facade.RequestsRequestService;
+import nl.tudelft.sem.template.users.facade.ResourcePoolRequestService;
 import nl.tudelft.sem.template.users.models.FacultyCreationRequestModel;
 import nl.tudelft.sem.template.users.models.FacultyCreationResponseModel;
 import nl.tudelft.sem.template.users.models.facade.DistributionModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -68,10 +70,15 @@ public class FacadeControllerTest {
     private ObjectMapper objectMapper;
     @Autowired
     private RequestSenderService requestSenderService;
+    @Autowired
+    private RequestsRequestService rqSender;
+    @Autowired
+    private NodesRequestService nodesSender;
+    @Autowired
+    private ResourcePoolRequestService rpSender;
 
     @MockBean
     private FacultyVerificationService facultyVerificationService;
-
     @MockBean
     private EmployeeRepository employeeRepository;
 
